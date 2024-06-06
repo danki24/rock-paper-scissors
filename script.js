39e98ea8afdcd13b6a34e7ec770c305a1260f2c4
danki24 <goullawilson742@gmail.com>
2024-06-06 10:07:37 +0000

    Create playRound function
    playRound function takes as parameters humanChoice, computerChoice compare them and conlole.log the winner

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
   let choice;
   const randomNumber = Math.floor(Math.random() * 3);
@@ -21,4 +24,46 @@ function getHumanChoice() {
   return playerChoice;
 }
 
-console.log(getHumanChoice());
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === humanChoice) {
+    console.log(`It's a tie! ${computerChoice} vs ${humanChoice}`);
+  } else {
+    if (computerChoice === "rock") {
+      if (humanChoice === "paper") {
+        console.log(`You win! Paper beats Rock`);
+        humanScore++;
+      }
+      if (humanChoice === "scissors") {
+        console.log(`You lose! Rock beats Scissors`);
+        computerScore++;
+      }
+    }
+
+    if (computerChoice === "paper") {
+      if (humanChoice === "rock") {
+        console.log(`You lose! Paper beats Rock`);
+        computerScore++;
+      }
+      if (humanChoice === "scissors") {
+        console.log(`You win! Scissors beats Paper`);
+        humanScore++;
+      }
+    }
+
+    if (computerChoice === "scissors") {
+      if (humanChoice === "paper") {
+        console.log(`You lose! Scissors beats Paper`);
+        computerScore++;
+      }
+      if (humanChoice === "rock") {
+        console.log(`You win! Rock beats Scissors`);
+        humanScore++;
+      }
+    }
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(computerSelection, humanSelection);
